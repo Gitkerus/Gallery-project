4d35bcdb216f89492924c1374180979836e475e7
Gitkerus <gitkerus@gmail.com>
2022-05-15 02:28:33 +0400

    maset

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
       <Header />
       <SearchBars />
       <Content />
+      <Pagination />
     </>
   );
 }
---
+++ src/Pagination.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Pagination() {
+  return (
+    <div className="pagination_box">
+      <button className="pagination_btn"></button>
+      <button className="pagination_btn"></button>
+      <button className="pagination_btn"></button>
+      <button className="pagination_btn"></button>
+      <button className="pagination_btn"></button>
+      <button className="pagination_btn"></button>
+      <button className="pagination_btn"></button>
+    </div>
+  );
+}
+export default Pagination;
