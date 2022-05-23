207d99b60dd487dfd16957ac5d22196b64558075
Gitkerus <gitkerus@gmail.com>
2022-05-23 19:09:51 +0400

    test commit

--- src/SearchBars.js
+++ src/SearchBars.js
@@ -1,14 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 import { Range, Select, Pagination, Input } from "fwt-internship-uikit";
 
+const options = [
+  {
+    id: 1,
+    name: "Author",
+  },
+  {
+    id: 2,
+    name: "Biftor",
+  },
+  {
+    id: 3,
+    name: "Tester",
+  },
+  {
+    id: 4,
+    name: "Sositer",
+  },
+  {
+    id: 5,
+    name: "test",
+  },
+  {
+    id: 6,
+    name: "test",
+  },
+  {
+    id: 7,
+    name: "test",
+  },
+  {
+    id: 8,
+    name: "test",
+  },
+  {
+    id: 9,
+    name: "test",
+  },
+  {
+    id: 10,
+    name: "test",
+  },
+  {
+    id: 11,
+    name: "test",
+  },
+];
+
 function SearchBars() {
+  const [value, setValue] = useState("Author");
+  const handleChange = (name) => {
+    setValue(name);
+  };
+
   return (
     <section className="search_box">
-      <div className="search_box_btn"></div>
-      {/* <Input className="search_box_btn" isDarkTheme={true} /> */}
-      <div className="search_box_btn"></div>
-      <div className="search_box_btn"></div>
-      <div className="search_box_btn"></div>
+      <Input isDarkTheme={true} />
+      <Select
+        isDarkTheme={true}
+        options={options}
+        onChange={handleChange}
+        value={value}
+      />
+      <Select isDarkTheme={true} />
+      <Range isDarkTheme={true} />
     </section>
   );
 }
