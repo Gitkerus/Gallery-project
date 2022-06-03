7cca21d5c4bdafa3bc9486dac3ae341922465371
Gitkerus <gitkerus@gmail.com>
2022-06-03 17:10:34 +0400

    Updates

---
+++ precache-manifest.4beaebfb3e15f83af4ad03c693120ac9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "eb6409519fdb6867f3e7",
+    "url": "/Gallery-project/static/css/main.3e8ee7c5.chunk.css"
+  },
+  {
+    "revision": "eb6409519fdb6867f3e7",
+    "url": "/Gallery-project/static/js/main.eb640951.chunk.js"
+  },
+  {
+    "revision": "54eb6dea101424bb8cfa",
+    "url": "/Gallery-project/static/css/1.29296322.chunk.css"
+  },
+  {
+    "revision": "54eb6dea101424bb8cfa",
+    "url": "/Gallery-project/static/js/1.54eb6dea.chunk.js"
+  },
+  {
+    "revision": "c597e58a8788d142089e",
+    "url": "/Gallery-project/static/js/runtime~main.c597e58a.js"
+  },
+  {
+    "revision": "8eed106c8f8d4dd99b0088afd9c23eb9",
+    "url": "/Gallery-project/static/media/logo.8eed106c.svg"
+  },
+  {
+    "revision": "3d03312baa5c37cf2a87504fe37e233d",
+    "url": "/Gallery-project/static/media/switcherLightTheme.3d03312b.svg"
+  },
+  {
+    "revision": "ad395d8decd89dce04c4df9cc2ed0a57",
+    "url": "/Gallery-project/static/media/switcherDarkTheme.ad395d8d.svg"
+  },
+  {
+    "revision": "287c0a41ed341a12891059b658c98b0a",
+    "url": "/Gallery-project/index.html"
+  }
+];
